9f23f8266737bb7f42230c39c28612aaf232dcef
eddie_xie <eddie_xie@derbysoft.com>
2019-11-17 15:39:05 +0800

    Modify DelegateExample

--- src/main/kotlin/com/derbysoft/kotlin/DelegateExample.kt
+++ src/main/kotlin/com/derbysoft/kotlin/DelegateExample.kt
@@ -21,3 +21,34 @@ class BarImpl : Bar {
 }
 
 class Derived(foo: Foo, bar: Bar) : Foo by foo, Bar by bar
+
+class MyList<T>(private val base:ArrayList<T>) : List<T> by base{
+    fun addIfMatch(element: T, matcher: Matcher<T>) {
+        if (matcher.match(element)) {
+            base.add(element)
+        }
+    }
+
+}
+
+interface Matcher<T> {
+    fun match(t:T):Boolean
+}
+
+fun main(args: Array<String>) {
+    val myList = MyList(ArrayList<String>())
+    val matcher: Matcher<String> = object : Matcher<String> {
+        override fun match(t: String): Boolean {
+            return t.matches(Regex("\\w*"))
+        }
+    }
+
+    myList.addIfMatch("XYZ", matcher)
+    myList.addIfMatch("123", matcher)
+    myList.forEach { println(it) }
+
+    val list = arrayListOf<String>()
+    list.add("XX")
+    list.add("123")
+    list.forEach { println(it) }
+}
