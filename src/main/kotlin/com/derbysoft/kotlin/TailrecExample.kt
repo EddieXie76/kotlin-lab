8910dda98c068f8f70ae2b9fc579de9cdcb81884
eddie_xie <eddie_xie@derbysoft.com>
2019-11-17 17:21:11 +0800

    Modify TailrecExample

--- src/main/kotlin/com/derbysoft/kotlin/TailrecExample.kt
+++ src/main/kotlin/com/derbysoft/kotlin/TailrecExample.kt
@@ -1,12 +1,25 @@
 package com.derbysoft.kotlin
 
-    tailrec fun sum(n: Int): Int {
-        if (n == 1) {
-            return 1;
-        }
-        return sum(n - 1) + 1
-    }
+tailrec fun sumNonTailRec(number: Int): Int = when {
+    number <= 1 -> 1
+    else -> number + sumNonTailRec(number - 1)
+}
+
+tailrec fun sum(number: Int, acc: Int = 0): Int = when {
+    number < 1 -> acc
+    else -> sum(number - 1, acc + number)
+}
+
+val eps = 1E-10
+
+tailrec fun findFixPoint(x: Double = 1.0): Double {
+    return if (Math.abs(x - Math.cos(x)) < eps) x else findFixPoint(Math.cos(x))
+}
 
 fun main(args: Array<String>) {
-    println(sum(5000000))
+
+    val number = 50000
+    println(sum(number))
+    println(sumNonTailRec(number))
+//    println(findFixPoint())
 }
