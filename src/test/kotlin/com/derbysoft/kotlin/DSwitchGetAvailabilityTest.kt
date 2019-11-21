eca0571eaaea7e4d9b5e40eea2632f0bde0250f6
eddie_xie <eddie_xie@derbysoft.com>
2019-11-21 13:33:28 +0800

    Add DSwitch example

--- src/test/kotlin/com/derbysoft/kotlin/DSwitchGetAvailabilityTest.kt
+++ src/test/kotlin/com/derbysoft/kotlin/DSwitchGetAvailabilityTest.kt
@@ -13,42 +13,11 @@ import org.junit.Test
 
 
 class DSwitchGetAvailabilityTest : AbstractTestSupport() {
-
     val oregonDSwitch = DefaultHotelBuyerRemoteService("54.186.187.63:9002-9008")
 
     @Test
     fun getAvailability() {
-
-//        val request:HotelAvailRequest = HotelAvailRequest(
-//                RequestHeader("MMT", "ACCOR", CodeGenerator.generate()),
-//                HotelAvailRQ(HotelAvailCriteriaDTO(
-//                        StayDateRangeDTO("2020-05-01","2020-05-02"),
-//                        RoomStayCandidateDTO( 1, 1, 0))))
-        val request: HotelAvailRequest = fromXml("""
-<com.derbysoft.dswitch.remote.hotel.dto.HotelAvailRequest>
-  <body class="com.derbysoft.dswitch.dto.hotel.avail.HotelAvailRQ">
-    <availCriteria>
-      <hotelCodes>
-        <string>6529</string>
-      </hotelCodes>
-      <stayDateRange>
-        <checkin>2020-05-01</checkin>
-        <checkout>2020-05-02</checkout>
-      </stayDateRange>
-      <roomStayCandidate>
-        <numberOfUnits>1</numberOfUnits>
-        <adultCount>1</adultCount>
-        <childCount>0</childCount>
-      </roomStayCandidate>
-    </availCriteria>
-  </body>
-  <header>
-    <taskId>TEST-${CodeGenerator.generate()}</taskId>
-    <source>MMT</source>
-    <destination>ACCOR</destination>
-  </header>
-</com.derbysoft.dswitch.remote.hotel.dto.HotelAvailRequest>
-     """.trimIndent())
+        val request = createAvailRequest()
         println(toXml(request))
         val response = oregonDSwitch.getNoCachedAvailability(request)
         println(toXml(response))
@@ -60,5 +29,34 @@ class DSwitchGetAvailabilityTest : AbstractTestSupport() {
                         "AAT:${it?.roomRate?.ratesList?.get(0)?.amountBeforeTax}")
             }
         }
+    }
+
+    private fun createAvailRequest(): HotelAvailRequest {
+        val request: HotelAvailRequest = fromXml("""
+    <com.derbysoft.dswitch.remote.hotel.dto.HotelAvailRequest>
+      <body class="com.derbysoft.dswitch.dto.hotel.avail.HotelAvailRQ">
+        <availCriteria>
+          <hotelCodes>
+            <string>6529</string>
+          </hotelCodes>
+          <stayDateRange>
+            <checkin>2020-05-01</checkin>
+            <checkout>2020-05-02</checkout>
+          </stayDateRange>
+          <roomStayCandidate>
+            <numberOfUnits>1</numberOfUnits>
+            <adultCount>1</adultCount>
+            <childCount>0</childCount>
+          </roomStayCandidate>
+        </availCriteria>
+      </body>
+      <header>
+        <taskId>TEST-${CodeGenerator.generate()}</taskId>
+        <source>MMT</source>
+        <destination>ACCOR</destination>
+      </header>
+    </com.derbysoft.dswitch.remote.hotel.dto.HotelAvailRequest>
+         """.trimIndent())
+        return request
     }
 }
